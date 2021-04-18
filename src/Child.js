312368fe1b468d3a80cce748b133002eac0c7df5
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-04-18 19:59:12 +0500

    consolelog output

--- src/Child.js
+++ src/Child.js
@@ -3,12 +3,11 @@ import counterContext from './counterContext'
 
 const Child =(props) =>{ 
     let counterValue = useContext(counterContext); 
-    document.write(counterValue);
+    console.log(counterValue);
     return(
         <div>
             <h3>This is first child</h3>
             <h2>My name is {props.name}</h2>
-            
 
         </div>
 
